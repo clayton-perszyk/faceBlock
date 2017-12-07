04f4163bd1eeb549918220d4c5e3e0c144568d6c
clayton-perszyk <clayperszyk@gmail.com>
2017-12-06 19:02:49 -0800

    add initial js code

--- js/main_popup.js
+++ js/main_popup.js
@@ -3,4 +3,20 @@
   //else show main_popup
 
 // In main_popup
-// get limit value and website name and store along with date
+// get limit value and website name and icon and store along with date
+$(document).ready(function(){
+  let tab;
+
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    tab = tabs[0];
+
+    $('#save').on('click', function() {
+      console.log(tab);
+
+      chrome.storage.sync.get('website')
+    });
+  });
+
+
+
+});
---
+++ js/secondary_popup.js
@@ -0,0 +1,12 @@
+// get current date
+// get date stored with website
+
+// if current date and queried date match
+  // decrement LimitMe
+  // if limit is zero
+    // show overlay with message
+  // else they have more views
+    // show notification with how many views left
+// if dates don't match match
+  // set date to today
+  // reset limit
