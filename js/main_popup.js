9f537ab1150e49dbc3d0f573f8a77e565634283f
clayton-perszyk <clayperszyk@gmail.com>
2017-12-13 12:23:31 -0800

    clean up main popup

--- js/main_popup.js
+++ js/main_popup.js
@@ -1,15 +1,3 @@
-// In main_popup
-// get limit value and website name and icon and store along with date
-
-// websites : [
-//     {name: facebook, limit: 2, icon: "http://...", date: '12/21/2001'},
-//     {name: facebook, limit: 2, icon: "http://...", date: '12/21/2001'},
-//     {name: facebook, limit: 2, icon: "http://...", date: '12/21/2001'}
-// ];
-
-// lm-facebook : {limit: 3, icon:  "http://...", date: '12/21/2001'}
-
-
 $(document).ready(function(){
   let tab,
       site,
@@ -17,48 +5,26 @@ $(document).ready(function(){
       icon,
       storageObj;
 
-
-  // check if website in storage
-   // if in storage show secondary_popup
-    //else show main_popup
-
   chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     tab = tabs[0];
+    site = ('lm-' + getDomain(tab.url));
 
+    // check if website in storage
+    chrome.storage.sync.get(site, function(items) {
+      // if in storage show secondary_popup
+        //else show main_popup
+      console.log(site);
+    });
 
 
     $('#save').on('click', function() {
-      // console.log(tab);
-      site = ('lm-' + getDomain(tab.url));
       limit = $('#limit').val();
       icon = tab.favIconUrl;
       storageObj = {};
       storageObj[site] = {'limit': limit, 'icon': icon, 'date': new Date().toDateString()};
-      console.log(storageObj)
 
-
-
-      // save name, limit, icon, date in storage
       chrome.storage.sync.set(storageObj);
 
-      chrome.storage.sync.get('lm-www.facebook.com', function(items) {
-        console.log("before: " + items['lm-www.facebook.com']['date']);
-      });
-
-      chrome.storage.sync.remove('lm-www.facebook.com', function(){
-        console.log("removed");
-      });
-
-      chrome.storage.sync.get('lm-www.facebook.com', function(items) {
-        if (items['lm-www.facebook.com']){
-          console.log("after: " + items['lm-www.facebook.com']['date']);
-        } else {
-          console.log("after: not here.");
-        }
-      });
     });
   });
-
-
-
 });
