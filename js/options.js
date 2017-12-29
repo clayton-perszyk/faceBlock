adc29ea3d4414002804f3134023e37e68dea24dd
clayton-perszyk <clayperszyk@gmail.com>
2017-12-29 12:32:06 -0800

    add enable/diable notifications to options

--- js/options.js
+++ js/options.js
@@ -1,8 +1,57 @@
+function toggleButtons(disableButton, enableButton) {
+  chrome.permissions.contains({
+    permissions: ['notifications']
+  }, function(result) {
+    if (result) {
+      enableButton.hide();
+      disableButton.show();
+    } else {
+      disableButton.hide();
+      enableButton.show();
+    }
+  });
+}
+
 $(document).ready(function(){
-  $('#notifications').on('click', function(e){
-    console.log("skd");
+  var $disableButton = $('#disable_notifications');
+  var $enableButton = $('#enable_notifications');
+
+  chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
+    if (info.status === "complete" && tabId) {
+      toggleButtons($disableButton, $enableButton);
+    }
+  });
+
+  chrome.tabs.onCreated.addListener(function() {
+    toggleButtons($disableButton, $enableButton);
+  });
+
+  chrome.tabs.onActivated.addListener(function() {
+    toggleButtons($disableButton, $enableButton);
+  });
+
+  chrome.windows.onFocusChanged.addListener(function(browserWindow) {
+    if (browserWindow != chrome.windows.WINDOW_ID_NONE) {
+      toggleButtons($disableButton, $enableButton);
+    }
+  });
+
+
+  $('#disable_notifications').on('click', function(e){
+    chrome.permissions.remove({
+     permissions: ['notifications']
+   }, function(result) {
+      $disableButton.hide();
+      $enableButton.show();
+    });
+  });
+
+  $('#enable_notifications').on('click', function(e){
     chrome.permissions.request({
      permissions: ['notifications']
-    }, function(granted) {});
+    }, function(granted) {
+        $enableButton.hide();
+        $disableButton.show();
+    });
   });
 });
