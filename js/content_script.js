d148ff6c454cd882f31eed51c15d0cadbb00c8b5
clayton-perszyk <clayperszyk@gmail.com>
2018-01-22 17:42:18 -0800

    add overlay on limit reached

--- js/content_script.js
+++ js/content_script.js
@@ -1,5 +1,27 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === "showOverlay") {
-    alert("limit reached!");
-  }
+$(document).ready(function(){
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "showOverlay") {
+      $("body").prepend(
+        `<div id="my-limit-overlay">
+          <div id="my-limit-content">
+            <p id="my-limit-text">You Have reached your limit for facebook.com</p>
+          </div>
+        </div>`
+      );
+
+      $('body').on({
+        "mousewheel": function(e) {
+          if (e.target.id == 'el') return;
+          e.preventDefault();
+          e.stopPropagation();
+        },
+        "keydown": function(e) {
+          console.log("asdfkllk");
+          if (e.target.id == 'el') return;
+          e.preventDefault();
+          e.stopPropagation();
+        }
+      });
+    }
+  });
 });
